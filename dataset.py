e2e411bb0d6a5c4b35974410c235e862c4393dc7
lumiere4758 <avijaileo@gmail.com>
2026-03-10 16:15:02 +0530

    copy dataset to dest

--- dataset.py
+++ dataset.py
@@ -1,6 +1,14 @@
 import kagglehub
+import shutil
+
 #Download latest version of mnist dataset
 
 path = kagglehub.dataset_download("hojjatk/mnist-dataset")
 
 print("downloaded to ", path)
+
+dest = "./dataset"
+
+shutil.move(path, dest)
+
+print("check", dest)
